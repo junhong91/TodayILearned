1f99266e83038618747464598ff0d4dd91d76f38
junhong91 <junhong199@gmail.com>
2022-07-10 21:25:19 +0900

    TypeAlias

---
+++ typescript/1-types/1-4-aliases.ts
@@ -0,0 +1,18 @@
+{
+  /**
+   * Type Aliases
+   * 새로운 Type을 정의한다
+   * TypeScript를 쓰는 이유, TypeScript의 꽃
+   */
+  type Text = string;
+  const name: Text = "JunHong";
+  const address: Text = "Korea";
+  type Student = {
+    name: string;
+    age: number;
+  };
+  const student: Student = {
+    name: "Junhong",
+    age: 32,
+  };
+}
