013f3a2b4a03c1594e7c97c7e286f32c49bb0bd0
junhong91 <junhong199@gmail.com>
2022-07-14 12:47:08 +0900

    CoffeMaker class

--- typescript/3-oop/3-2-class.ts
+++ typescript/3-oop/3-2-class.ts
@@ -1,28 +1,30 @@
-type CoffeeCup = {
-  shots: number;
-  hasMilk: boolean;
-};
-class CoffeeMaker {
-  static BEANS_GRAMM_PER_SHOT: number = 7; // static level
-  coffeeBeans: number; // instance (object) level
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+  class CoffeeMaker {
+    static BEANS_GRAMM_PER_SHOT: number = 7; // static level
+    coffeeBeans: number; // instance (object) level
 
-  constructor(coffeeBeans: number) {
-    this.coffeeBeans = coffeeBeans;
-  }
-
-  makeCoffee(shots: number): CoffeeCup {
-    if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-      throw new Error("Not enough coffee beans!");
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
     }
-    this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
-    return {
-      shots,
-      hasMilk: false,
-    };
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
   }
+
+  const coffeeMaker: CoffeeMaker = new CoffeeMaker(21);
+
+  const coffeeCup: CoffeeCup = coffeeMaker.makeCoffee(2);
+  console.log(coffeeCup);
 }
-
-const coffeeMaker: CoffeeMaker = new CoffeeMaker(21);
-
-const coffeeCup: CoffeeCup = coffeeMaker.makeCoffee(2);
-console.log(coffeeCup);
