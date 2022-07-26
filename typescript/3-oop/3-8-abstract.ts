b4717ee89391c79dacf76199ce555bef5395ac1e
junhong91 <junhong199@gmail.com>
2022-07-27 08:01:28 +0900

    TS - abstract

---
+++ typescript/3-oop/3-8-abstract.ts
@@ -0,0 +1,81 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  abstract class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; // static level
+    private coffeeBeans: number; // instance (object) level
+
+    public constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    clean() {
+      console.log("Cleaning the machine...");
+    }
+
+    private preheat(): void {
+      console.log("heating up...");
+    }
+
+    protected abstract extract(shots: number): CoffeeCup;
+       
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    private steamMilk(): void {
+      console.log("Steaming some milk...");
+    }
+      
+    protected extract(shots: number): CoffeeCup {
+        this.steamMilk();
+        return { shots, hasMilk: true };
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    protected extract(shots: number): CoffeeCup {
+        return {
+        shots,
+        hasSugar: true,
+      };
+    }
+  }
+
+  const machines: CoffeeMachine[] = [
+    new CaffeLatteMachine(16),
+    new SweetCoffeeMaker(16),
+  ];
+  // Polymorphism
+  machines.forEach((machine) => {
+    console.log("----------------------------");
+    machine.makeCoffee(1);
+  });
+}
