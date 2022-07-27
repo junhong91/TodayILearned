da377e0dff108b9b7088848fdab819d999dec82d
junhong91 <junhong199@gmail.com>
2022-07-27 14:08:46 +0900

    TS - GENERIC

--- typescript/5-generic/5-2-class.ts
+++ typescript/5-generic/5-2-class.ts
@@ -1,5 +1,20 @@
 // either: a or b
-interface Either {
-  left: () => number;
-  right: () => number;
+interface Either<L, R> {
+  left: () => L;
+  right: () => R;
 }
+
+class SimpleEither<L, R> implements Either<L, R> {
+  constructor(private leftValue: L, private rightValue: R) {}
+  left(): L {
+    return this.leftValue;
+  }
+  right(): R {
+    return this.rightValue;
+  }
+}
+
+const either: Either<number, number> = new SimpleEither(4, 5);
+either.left(); // 4
+either.right(); // 5
+const best: Either<string, number> = new SimpleEither("best", 5);
