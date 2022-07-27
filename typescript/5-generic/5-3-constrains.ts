c07bd07ef8e2a0a0541bba232e2dad640b2af64e
junhong91 <junhong199@gmail.com>
2022-07-27 14:35:47 +0900

    Generic constrains

---
+++ typescript/5-generic/5-3-constrains.ts
@@ -0,0 +1,44 @@
+interface Employee {
+  pay(): void;
+}
+
+class FullTimeEmployee implements Employee {
+  pay() {
+    console.log("Full time!");
+  }
+  workFullTime() {}
+}
+
+class PartTimeEmployee implements Employee {
+  pay() {
+    console.log("Part time!");
+  }
+  workPartTime() {}
+}
+
+// 세부적인 타입을 인자로 받아서 추상적인 타입(Employee)으로 다시 리턴하는 함수는
+// 매우 좋지 않음
+function payBad(employee: Employee): Employee {
+  employee.pay();
+  return employee;
+}
+
+// Constraint!
+// <T extends Employee>는 Generic이긴 Generic인데 Employee를 확장한 Type만 가능
+function pay<T extends Employee>(employee: T): T {
+  employee.pay();
+  return employee;
+}
+
+const junhong = new FullTimeEmployee();
+const bob = new PartTimeEmployee();
+junhong.workFullTime();
+bob.workPartTime();
+
+// const junhongAfterPay = payBad(junhong);
+// junhongAfterPay.workFullTime(); <- Invalid!
+// or you can use `as` keyword : Type assertion
+// const junhongAfterPay = pay(junhong) as FullTimeEmployee; <- But 역시 좋지 않은 방식
+
+const junhongAfterPay = pay(junhong);
+junhongAfterPay.workFullTime();
